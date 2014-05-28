fa54d9438ae1df6a521815dd40d63fc494c28963
Joris Vandeweerd <joris@statik.be>
2014-05-28 11:54:16 +0200

    removed console.log from toc js

--- public/js/cms/text.toc.js
+++ public/js/cms/text.toc.js
@@ -1,12 +1,10 @@
 $(function() {
     $('.widget-text-toc').each(function() {
         var divIndex = $(this);
-        console.log(divIndex);
         var index = $('<ul></ul>');
 
         $('.widget h2.toc', divIndex.parents('.region')).each(function() {
             var title = $(this);
-          console.log(title);
             var label = title.data('label-menu');
 
             if (label == undefined) {
